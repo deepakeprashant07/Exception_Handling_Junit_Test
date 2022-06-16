4c863d13826f9456263404e9acf35edb38e080bd
Prashant Deepake <deepakeprashant1@gmail.com>
2022-06-16 19:22:12 +0530

    ADDED: RETURN HAPPY MESSAGE USING TEST CASE 1.2

--- Test/MoodAnalyserTest.java
+++ Test/MoodAnalyserTest.java
@@ -2,9 +2,16 @@ import org.junit.Test;
 
 public class MoodAnalyserTest {
     @Test
-    public void testMoodAnalysis () {
+    public void testMoodAnalysisReturnSad () {
         MoodAnalyser moodAnalyser = new MoodAnalyser();
         String mood = moodAnalyser.analyseMood("I am in Sad Mood");
+        System.out.println(mood);
+    }
+
+    @Test
+    public void testMoodAnalysisReturnHappy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("I am in Any Mood");
         System.out.println(mood);
     }
 }
